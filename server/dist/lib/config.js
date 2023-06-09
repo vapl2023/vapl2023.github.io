a131cd343030057aeb02ecd89f91c719182cc0e6
Neal Wang <nealwang.sh@protonmail.com>
2023-06-08 23:51:02 -0700

    remove dist

--- server/dist/lib/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BULLY_POP = exports.BULLY_TAKE = exports.BULLY_GET = void 0;
-exports.BULLY_GET = 4;
-exports.BULLY_TAKE = 10;
-exports.BULLY_POP = 0.5;
